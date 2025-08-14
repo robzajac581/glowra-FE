0cabc8b3d9eb33fc826659efffce3e6e8df38897
robzajac581 <rzajac581@gmail.com>
2025-08-14 09:12:18 -0400

    Removed decimals from clinic price numbers

--- src/pages/clinic/components/ClinicProcedures.jsx
+++ src/pages/clinic/components/ClinicProcedures.jsx
@@ -65,8 +65,8 @@ const ClinicProcedureTable = ({
 		return new Intl.NumberFormat('en-US', {
 			style: 'currency',
 			currency: 'USD',
-			minimumFractionDigits: 2,
-			maximumFractionDigits: 2,
+			minimumFractionDigits: 0,
+			maximumFractionDigits: 0,
 		}).format(price);
 	};
 
